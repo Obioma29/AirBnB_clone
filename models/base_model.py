803914600d5b523c832d4f3d5a4f87ab335ea3d2
obioma29 <obiomachinwuko@yahoo.com>
2023-05-15 03:48:54 -0700

    AirBnB project

--- models/base_model.py
+++ models/base_model.py
@@ -19,14 +19,12 @@ class BaseModel:
 
         if kwargs is not None and kwargs != {}:
             for k, v in kwargs.items():
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                if k == "created_at":
+                    self.__dict__[k] = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                elif k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
                 else:
-                    self.__dict__[key] = v
+                    self.__dict__[k] = v
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
@@ -50,6 +48,6 @@ class BaseModel:
         """Returns a dictionary representation of self"""
         temp = {**self.__dict__}
         temp['__class__'] = type(self).__name__
-        temp['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        temp['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        temp['created_at'] = self.created_at.isoformat()
+        temp['updated_at'] = self.updated_at.isoformat()
         return temp
