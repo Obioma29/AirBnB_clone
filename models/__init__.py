fa3e9d0117e6d6ac6d046723b157e0937ea4c437
obioma29 <obiomachinwuko@yahoo.com>
2023-05-15 03:23:54 -0700

    AirBnB project

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""
-__init__ dunder method for the models directory
-"""
-from models.engine.file_storage import FileStorage
+"""__init__  method for the models directory"""
 
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
 
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
@@ -21,29 +21,28 @@ class BaseModel:
             for k, v in kwargs.items():
                 if key == "created_at":
                     self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
                     self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = v
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """String representation of self"""
 
         fmt = "[{}] ({}) {}"
-        return fmt.format(
-                type(self).__name__,
-                self.id,
+        return fmt.format(type(self).__name__, self.id,
                 self.__dict__)
 
     def save(self):
         """updates last updated variable"""
+
         self.updated_at = datetime.now()
         models.storage.save()
 
